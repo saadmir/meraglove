4892677e8270455f896d65056766833b9196d299
Saad Mir <smir@vmware.com>
2017-07-26 11:49:56 -0700

    ported from androidprojects dir

---
+++ app/src/main/java/com/meraglove/meraglove/Constants.java
@@ -0,0 +1,5 @@
+package com.meraglove.meraglove;
+
+public class Constants {
+    public static final String TAG = ">>>> MeraGlove";
+}
--- app/src/main/java/meraglove/com/meraglove/Constants.java
+++
@@ -1,8 +0,0 @@
-package meraglove.com.meraglove;
-
-/**
- * Created by mwoolley on 20/11/2015.
- */
-public class Constants {
-    public static final String TAG = "New Profile";
-}
